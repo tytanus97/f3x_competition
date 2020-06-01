43654166fa8af54bf89bd94c7e384c7f50968ee8
tytanus97 <tytanus97@gmail.com>
2020-06-01 15:50:22 +0200

    add round service logic

--- src/main/java/f3x/competition/F3XCompetition/service/RoundService.java
+++ src/main/java/f3x/competition/F3XCompetition/service/RoundService.java
@@ -1,4 +1,16 @@
 package f3x.competition.F3XCompetition.service;
 
+import f3x.competition.F3XCompetition.entity.Flight;
+import f3x.competition.F3XCompetition.entity.Round;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface RoundService {
+    List<Round> getAll();
+    Optional<Round> getById(Long id);
+    List<Flight> getRoundFlights(Round round);
+    void addFlightToRound(Round round,Flight flight);
+    void removeFlightFromRound(Round round,Flight flight);
+
 }
--- src/main/java/f3x/competition/F3XCompetition/serviceImpl/RoundServiceImpl.java
+++ src/main/java/f3x/competition/F3XCompetition/serviceImpl/RoundServiceImpl.java
@@ -1,8 +1,60 @@
 package f3x.competition.F3XCompetition.serviceImpl;
 
+import f3x.competition.F3XCompetition.entity.Flight;
+import f3x.competition.F3XCompetition.entity.Round;
+import f3x.competition.F3XCompetition.repository.FlightRepository;
+import f3x.competition.F3XCompetition.repository.RoundRepository;
 import f3x.competition.F3XCompetition.service.RoundService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RoundServiceImpl  implements RoundService {
+
+    private final RoundRepository roundRepository;
+    private final FlightRepository flightRepository;
+
+    @Autowired
+    public RoundServiceImpl(RoundRepository roundRepository, FlightRepository flightRepository) {
+        this.roundRepository = roundRepository;
+        this.flightRepository = flightRepository;
+    }
+
+    @Override
+    @Transactional
+    public List<Round> getAll() {
+        return this.roundRepository.findAll();
+    }
+
+    @Override
+    @Transactional
+    public Optional<Round> getById(Long roundId) {
+        return this.roundRepository.findById(roundId);
+    }
+
+    @Override
+    @Transactional
+    public List<Flight> getRoundFlights(Round round) {
+        return round.getRoundFlights();
+    }
+
+    @Override
+    @Transactional
+    public void addFlightToRound(Round round, Flight flight) {
+        round.addFlight(flight);
+        this.roundRepository.save(round);
+        this.flightRepository.save(flight);
+    }
+
+    @Override
+    @Transactional
+    public void removeFlightFromRound(Round round, Flight flight) {
+            round.removeFlight(flight);
+        this.roundRepository.save(round);
+        this.flightRepository.save(flight);
+    }
 }
